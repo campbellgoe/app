a2a9547f4e390dc53761474be81c20ecae527ed8
George Campbell <campbell.goe@gmail.com>
2023-01-11 19:54:57 +0000

    start on Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,78 @@
 import React from "react";
-// import styles from '../App.module.scss';
+import Title from "../components/Title";
 
-class Dashboard extends React.Component {
+type RangeIndexSelectorProps = {
+  maxItems: number;
+  selectedIndex: number;
+  setSelectedIndex: (newIndex: number) => void
+}
+
+class RangeIndexSelector extends React.Component<RangeIndexSelectorProps> {
   render(){
-    // const { container, header, main, footer } = styles
+    const { maxItems, setSelectedIndex, selectedIndex } = this.props
     return (
-      <div>Hello dashboard</div>
-      // <div className={container}>
-      //   <header className={header}>
-          
-      //   </header>
-      //   <main className={main}></main>
-      //   <footer className={footer}></footer>
-      // </div>
+      <input
+        type="range"
+        min={0}
+        max={maxItems - 1}
+        step={1}
+        value={selectedIndex}
+        onChange={e => setSelectedIndex(parseInt(e.target.value))}
+      />
+    )
+  }
+}
+type DashboardStateType = {
+  loaded: boolean;
+  data: {
+    title: string
+    attributes: {
+      name: string;
+      value: number;
+      unit: string;
+    }[]
+  }[];
+  selectedIndex: number
+}
+class Dashboard extends React.Component<{}, DashboardStateType> {
+
+  state = {
+    loaded: false,
+    data: [],
+    selectedIndex: 0
+  }
+
+  componentDidMount() {
+    const fetchAndSetData = async () => {
+      const res = await fetch('/data.json')
+      const data = await res.json()
+      this.setState({ data, loaded: true, selectedIndex: 0 })
+    }
+    fetchAndSetData()
+  }
+  render(){
+    if(!this.state.loaded) {
+      return (
+        <div>Loading...</div>
+      )
+    }
+    const { data, selectedIndex } = this.state
+    const item = data[selectedIndex]
+    const { title, attributes } = item
+    return (
+      <div>
+        <Title>{title}</Title>
+        <RangeIndexSelector
+        maxItems={data.length}
+        setSelectedIndex={(newIndex: number) => {
+          this.setState(state => ({
+            ...state,
+            selectedIndex: newIndex
+          }))
+        }}
+        selectedIndex={selectedIndex}
+        />
+      </div>
     )
   }
 }
